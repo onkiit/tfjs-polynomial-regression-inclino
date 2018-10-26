a0a76437996050c2b19738c96e1bab1b915a2356
Tikno <tikno.edu@gmail.com>
2018-10-26 16:59:35 +0700

    initial commit

---
+++ sensor.js
@@ -0,0 +1,403 @@
+export default {
+  "data": [{
+      "displacement": 0.5,
+      "DSPA": "3.549999999999998"
+    },
+    {
+      "displacement": 1.0,
+      "DSPA": "3.379999999999998"
+    },
+    {
+      "displacement": 1.5,
+      "DSPA": "3.189999999999998"
+    },
+    {
+      "displacement": 2.0,
+      "DSPA": "2.879999999999998"
+    },
+    {
+      "displacement": 2.5,
+      "DSPA": "2.549999999999998"
+    },
+    {
+      "displacement": 3.0,
+      "DSPA": "2.209999999999998"
+    },
+    {
+      "displacement": 3.5,
+      "DSPA": "1.9299999999999982"
+    },
+    {
+      "displacement": 4.0,
+      "DSPA": "1.6199999999999983"
+    },
+    {
+      "displacement": 4.5,
+      "DSPA": "1.5799999999999983"
+    },
+    {
+      "displacement": 5.0,
+      "DSPA": "1.5899999999999983"
+    },
+    {
+      "displacement": 5.5,
+      "DSPA": "1.5999999999999983"
+    },
+    {
+      "displacement": 6.0,
+      "DSPA": "1.6899999999999984"
+    },
+    {
+      "displacement": 6.5,
+      "DSPA": "1.7999999999999983"
+    },
+    {
+      "displacement": 7.0,
+      "DSPA": "1.859999999999998"
+    },
+    {
+      "displacement": 7.5,
+      "DSPA": "2.189999999999998"
+    },
+    {
+      "displacement": 8.0,
+      "DSPA": "2.649999999999998"
+    },
+    {
+      "displacement": 8.5,
+      "DSPA": "3.129999999999998"
+    },
+    {
+      "displacement": 9.0,
+      "DSPA": "3.309999999999998"
+    },
+    {
+      "displacement": 9.5,
+      "DSPA": "3.3999999999999977"
+    },
+    {
+      "displacement": 10.0,
+      "DSPA": "3.4099999999999984"
+    },
+    {
+      "displacement": 10.5,
+      "DSPA": "3.2999999999999985"
+    },
+    {
+      "displacement": 11.0,
+      "DSPA": "3.339999999999998"
+    },
+    {
+      "displacement": 11.5,
+      "DSPA": "3.2499999999999982"
+    },
+    {
+      "displacement": 12.0,
+      "DSPA": "3.539999999999998"
+    },
+    {
+      "displacement": 12.5,
+      "DSPA": "3.809999999999998"
+    },
+    {
+      "displacement": 13.0,
+      "DSPA": "4.129999999999997"
+    },
+    {
+      "displacement": 13.5,
+      "DSPA": "4.469999999999998"
+    },
+    {
+      "displacement": 14.0,
+      "DSPA": "4.969999999999998"
+    },
+    {
+      "displacement": 14.5,
+      "DSPA": "5.449999999999998"
+    },
+    {
+      "displacement": 15.0,
+      "DSPA": "5.939999999999999"
+    },
+    {
+      "displacement": 15.5,
+      "DSPA": "6.369999999999999"
+    },
+    {
+      "displacement": 16.0,
+      "DSPA": "6.85"
+    },
+    {
+      "displacement": 16.5,
+      "DSPA": "7.459999999999999"
+    },
+    {
+      "displacement": 17.0,
+      "DSPA": "7.799999999999999"
+    },
+    {
+      "displacement": 17.5,
+      "DSPA": "8.29"
+    },
+    {
+      "displacement": 18.0,
+      "DSPA": "8.54"
+    },
+    {
+      "displacement": 18.5,
+      "DSPA": "8.75"
+    },
+    {
+      "displacement": 19.0,
+      "DSPA": "8.709999999999999"
+    },
+    {
+      "displacement": 19.5,
+      "DSPA": "8.729999999999999"
+    },
+    {
+      "displacement": 20.0,
+      "DSPA": "8.739999999999998"
+    },
+    {
+      "displacement": 20.5,
+      "DSPA": "8.659999999999998"
+    },
+    {
+      "displacement": 21.0,
+      "DSPA": "8.559999999999997"
+    },
+    {
+      "displacement": 21.5,
+      "DSPA": "8.449999999999998"
+    },
+    {
+      "displacement": 22.0,
+      "DSPA": "8.439999999999996"
+    },
+    {
+      "displacement": 22.5,
+      "DSPA": "8.369999999999997"
+    },
+    {
+      "displacement": 23.0,
+      "DSPA": "8.529999999999998"
+    },
+    {
+      "displacement": 23.5,
+      "DSPA": "8.669999999999998"
+    },
+    {
+      "displacement": 24.0,
+      "DSPA": "8.799999999999997"
+    },
+    {
+      "displacement": 24.5,
+      "DSPA": "8.919999999999998"
+    },
+    {
+      "displacement": 25.0,
+      "DSPA": "9.069999999999997"
+    },
+    {
+      "displacement": 25.5,
+      "DSPA": "9.359999999999998"
+    },
+    {
+      "displacement": 26.0,
+      "DSPA": "9.489999999999998"
+    },
+    {
+      "displacement": 26.5,
+      "DSPA": "9.539999999999997"
+    },
+    {
+      "displacement": 27.0,
+      "DSPA": "9.529999999999998"
+    },
+    {
+      "displacement": 27.5,
+      "DSPA": "9.389999999999997"
+    },
+    {
+      "displacement": 28.0,
+      "DSPA": "9.249999999999996"
+    },
+    {
+      "displacement": 28.5,
+      "DSPA": "9.039999999999997"
+    },
+    {
+      "displacement": 29.0,
+      "DSPA": "8.979999999999997"
+    },
+    {
+      "displacement": 29.5,
+      "DSPA": "8.859999999999996"
+    },
+    {
+      "displacement": 30.0,
+      "DSPA": "8.639999999999997"
+    },
+    {
+      "displacement": 30.5,
+      "DSPA": "8.499999999999996"
+    },
+    {
+      "displacement": 31.0,
+      "DSPA": "8.469999999999997"
+    },
+    {
+      "displacement": 31.5,
+      "DSPA": "8.089999999999998"
+    },
+    {
+      "displacement": 32.0,
+      "DSPA": "7.859999999999998"
+    },
+    {
+      "displacement": 32.5,
+      "DSPA": "7.709999999999998"
+    },
+    {
+      "displacement": 33.0,
+      "DSPA": "7.409999999999998"
+    },
+    {
+      "displacement": 33.5,
+      "DSPA": "7.139999999999998"
+    },
+    {
+      "displacement": 34.0,
+      "DSPA": "6.789999999999998"
+    },
+    {
+      "displacement": 34.5,
+      "DSPA": "6.479999999999999"
+    },
+    {
+      "displacement": 35.0,
+      "DSPA": "6.129999999999999"
+    },
+    {
+      "displacement": 35.5,
+      "DSPA": "5.849999999999999"
+    },
+    {
+      "displacement": 36.0,
+      "DSPA": "5.979999999999999"
+    },
+    {
+      "displacement": 36.5,
+      "DSPA": "5.839999999999999"
+    },
+    {
+      "displacement": 37.0,
+      "DSPA": "6.019999999999999"
+    },
+    {
+      "displacement": 37.5,
+      "DSPA": "5.099999999999999"
+    },
+    {
+      "displacement": 38.0,
+      "DSPA": "4.929999999999999"
+    },
+    {
+      "displacement": 38.5,
+      "DSPA": "4.6499999999999995"
+    },
+    {
+      "displacement": 39.0,
+      "DSPA": "4.46"
+    },
+    {
+      "displacement": 39.5,
+      "DSPA": "4.36"
+    },
+    {
+      "displacement": 40.0,
+      "DSPA": "4.24"
+    },
+    {
+      "displacement": 40.5,
+      "DSPA": "3.9600000000000004"
+    },
+    {
+      "displacement": 41.0,
+      "DSPA": "3.58"
+    },
+    {
+      "displacement": 41.5,
+      "DSPA": "3.18"
+    },
+    {
+      "displacement": 42.0,
+      "DSPA": "2.96"
+    },
+    {
+      "displacement": 42.5,
+      "DSPA": "2.55"
+    },
+    {
+      "displacement": 43.0,
+      "DSPA": "2.01"
+    },
+    {
+      "displacement": 43.5,
+      "DSPA": "1.89"
+    },
+    {
+      "displacement": 44.0,
+      "DSPA": "1.6199999999999999"
+    },
+    {
+      "displacement": 44.5,
+      "DSPA": "1.39"
+    },
+    {
+      "displacement": 45.0,
+      "DSPA": "1.22"
+    },
+    {
+      "displacement": 45.5,
+      "DSPA": "1.04"
+    },
+    {
+      "displacement": 46.0,
+      "DSPA": "0.20000000000000007"
+    },
+    {
+      "displacement": 46.5,
+      "DSPA": "0.20000000000000007"
+    },
+    {
+      "displacement": 47.0,
+      "DSPA": "0.08000000000000007"
+    },
+    {
+      "displacement": 47.5,
+      "DSPA": "0.4"
+    },
+    {
+      "displacement": 48.0,
+      "DSPA": "0.06"
+    },
+    {
+      "displacement": 48.5,
+      "DSPA": "0.020000000000000018"
+    },
+    {
+      "displacement": 49.0,
+      "DSPA": "-0.03999999999999998"
+    },
+    {
+      "displacement": 49.5,
+      "DSPA": "0.14"
+    },
+    {
+      "displacement": 50.0,
+      "DSPA": "-0.08"
+    }
+  ]
+}
